496d0662b63d3501407aa9abcfb1699a77fdef45
LI JIAHAO <lijiahao99131@gmail.com>
2018-03-06 19:36:09 +0800

    handle the mkv problem
    Requested formats are incompatible for merge and will be merged into mkv.
    https://github.com/rg3/youtube-dl/issues/5710

--- download.py
+++ download.py
@@ -4,6 +4,7 @@
 from youtube_dl import YoutubeDL
 import sys
 import logging
+import os
 
 if len(sys.argv) <= 1:
     logging.error("No url specified")
@@ -13,8 +14,13 @@ url = sys.argv[1]
 
 print(url)
 
-with YoutubeDL({'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio",  'outtmpl': '%(title)s.%(ext)s'}) as ydl:
+with YoutubeDL({'format': "bestvideo+bestaudio/bestvideo[ext=mp4]+bestaudio[ext=m4a]/best",  'outtmpl': '%(title)s.%(ext)s'}) as ydl:
     info_dict = ydl.extract_info(url, download=True)
     fn = ydl.prepare_filename(info_dict)
 
-print("fn:\"" + fn + "\"")
+if os.path.exists(fn):
+    print("fn:\"" + fn + "\"")
+elif os.path.exists(os.path.splitext(fn)[0] + '.mkv'):
+    print("fn:\"" + os.path.splitext(fn)[0] + '.mkv' + "\"")
+else:
+    print("Error: file doesn't exist")
